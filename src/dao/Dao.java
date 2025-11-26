3ab24d78adb0d4f5624c7b9a9837d5fc14cd4fe0
Albora Izquierdo Hernández <148754699+Alboraizquiher@users.noreply.github.com>
2025-11-26 18:43:20 +0100

    Añadido de los nuevos metodos: Insertar Actualizar y Eliminar

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -1,21 +1,24 @@
 package dao;
 
 import java.util.List;
-
 import model.Employee;
 import model.Product;
 
 public interface Dao {
-	
-	public void connect();
+    
+    public void connect();
+    public void disconnect();
 
-	public void disconnect();
-	
+    // Login empleado
+    public Employee getEmployee(int employeeId, String password);
+    
+    // Inventario
+    public List<Product> getInventory();
+    public boolean writeInventory(List<Product> inventory);
 
-	public Employee getEmployee(int employeeId, String password);
-	
-	//NUEVOS METODOS INCORPORADOS
-	
-	public List<Product> getInventory();
-	public boolean writeInventory(List<Product> inventory);
+    // NUEVOS MÉTODOS
+    public boolean addProduct(Product product);   // Insertar nuevo producto
+    public boolean updateProduct(Product product); // Actualizar stock o datos
+    public boolean deleteProduct(int productId);   // Eliminar producto
+
 }
